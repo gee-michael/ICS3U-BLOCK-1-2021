0fdf37ed216892453220c80b43f7d6ffa83cd538
Michael Gee <mgee@bayviewglen.ca>
2021-09-09 09:30:03 -0400

    Initial Commit for Unit 1

---
+++ Unit 1 - Introduction to CS/src/week1/ExampleOne.java
@@ -0,0 +1,20 @@
+package week1;
+
+//every file will be its own class FOR NOW
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+    public static void main(String[] args) {
+        //the main method (function) lets Java know where to start.
+
+        //System.out.println writes to the console.
+        //Whatever is in the round brackets goes in the console.
+        System.out.println("Welcome to ICS3U AP!");
+        System.out.println("Welcome to ICS3U AP!");
+        System.out.println("Welcome to ICS3U AP!");
+        System.out.println("Welcome to ICS3U AP!");
+        System.out.println("Welcome to ICS3U AP!");
+    }
+}
---
+++ Unit 1 - Introduction to CS/src/week1/ExampleTwo.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+        System.out.print("D");
+        System.out.print("E");
+    }
+}
