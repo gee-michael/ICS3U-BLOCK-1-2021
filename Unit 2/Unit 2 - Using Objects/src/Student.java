e7babcd2312300b3265b24fada1bf11f49d42b33
Michael Gee <mgee@bayviewglen.ca>
2021-09-16 09:33:02 -0400

    Student Example Code

---
+++ Unit 2/Unit 2 - Using Objects/src/Student.java
@@ -0,0 +1,74 @@
+/**
+ * A class is a blueprint for something that we can model and create variables to use in our code.
+ * This code (class) defines what a student is.
+ */
+
+public class Student {
+
+    /**
+     * Attributes that define a student
+     * The state of a specific student is what's in the attributes at a specific point
+     */
+
+    private String name;
+    private String studentNumber;
+    private int grade;
+    private int totalMarks;
+    private int numMarks;
+    private double average;
+
+    /**
+     * The method with the same name as the class is used to instantiate (create) a student object.
+     * 
+     * Constructor - same name as the class and its purpose is to instantiate (create) an instance of a class.
+     */
+
+    public Student(String name, String studentNumber, int grade) {
+        this.name = name;
+        this.studentNumber = studentNumber;
+        this.grade = grade;
+        this.totalMarks = 0;
+        this.numMarks = 0;
+        this.average = 0;
+    }
+
+    /**
+     * displayName, displayStudentNumber, increaseGrade, displayGrade
+     * The methods (actions) in a class define its behaviour
+     */
+
+    public void displayName() {
+        System.out.println(name);
+    }
+
+    /**
+     *  A void method performs a task and does NOT return a value.
+     *  void -> action (e.g. write to console);
+     */
+
+    public void displayStudentNumber() {
+        System.out.println(studentNumber);
+    }
+
+    public void increaseGrade() {
+        grade++;
+    }
+
+    public void displayGrade() {
+        System.out.println(grade);
+    }
+    
+    public void displayAverage(){
+        System.out.println(average);
+    }
+
+    public void addTest(int mark){
+        totalMarks += mark;
+        numMarks++;
+        calcAverage();
+    }
+
+    public void calcAverage(){
+        average = (double) totalMarks / numMarks;
+    }
+}
---
+++ Unit 2/Unit 2 - Using Objects/src/StudentExample.java
@@ -0,0 +1,45 @@
+public class StudentExample {
+    public static void main (String[] args) {       
+        Student shohei;
+        shohei = new Student("Shohei", "123456", 11); 
+        // shohei is a student (object variable, not primitive)
+
+        Student samantha = new Student("Samantha", "654321", 11);
+        // primitive variables store the value
+        // 'new Student' assigns a value, 'student []' creates a reference to the values given
+        // strings are NOT primitive, they are objects
+
+        Student alan = shohei; // alan is now at shohei's reference. (address)
+        alan.increaseGrade(); // called the increaseGrade() funtion
+        // since alan and shohei reference the same object, BOTH move up
+
+        // we activate methods (functions) through the variable name ( [name].[function](); )
+
+        samantha.displayName();
+        samantha.displayStudentNumber();
+        samantha.increaseGrade();
+
+        samantha = null; // everything stored in samantha is now gone !
+
+        // samantha.displayGrade(); NULL POINTER EXCEPTION since 'samantha' points to null
+        // NON-Static means it belongs to the object (each object gets a copy)
+
+        alan = new Student("Alan", "55555", 11);
+
+        alan.addTest(87);
+        alan.displayAverage();
+        alan.addTest(92);
+        alan.displayAverage();
+        alan.addTest(96);
+        alan.displayAverage();
+        alan.addTest(67);
+        alan.displayAverage();
+
+        // alan.numMarks = 7; don't have access to PRIVATE members
+    }    
+}
+
+
+
+
+
