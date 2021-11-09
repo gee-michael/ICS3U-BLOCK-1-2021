9085feaf87732ff80f207731f6ef067eb21b82fb
Michael Gee <mgee@bayviewglen.ca>
2021-11-09 10:16:47 -0500

    some stuff

--- Unit 6 - Arrays and Array Lists/src/week10/Animal.java
+++ Unit 6 - Arrays and Array Lists/src/week10/Animal.java
@@ -1,5 +1,22 @@
 package week10;
 
 public class Animal {
+    public static void main(String[] args){
+        accessArray();
+        iterateAnArray();
+    }
 
+    private static void accessArray() {
+        int[] arr = { 5, 3, 2, 5, 2 };
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+
+    private static void iterateAnArray() {
+        int[] arr = new int[5];
+        Animal[] animals = new Animal[3];
+    }
 }
